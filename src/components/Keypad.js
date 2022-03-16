c3f184972b0df65df4dee6e3ec8faab337fbf6a6
Alyssa Falcione <lyzarddz@gmail.com>
2022-03-15 22:02:14 -0400

    Completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
 // Code Keypad Component Here
+
+import React from "react";
+
+function Keypad(){
+    function handleChange(event){
+        event.preventDefault()
+        console.log('Entering password...');
+    }
+
+    return (
+    <input
+    type="password"
+    onChange={handleChange}
+    placeholder="Input here"
+    />    
+    )
+}
+
+    export default Keypad;
